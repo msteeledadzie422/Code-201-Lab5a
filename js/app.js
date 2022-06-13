168d5a5d6e2c54bb3f747ddb5073b1f98386c867
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-06-12 20:31:11 -0600

    pulling remote changes in Main branch down to local

--- js/app.js
+++ js/app.js
@@ -2,14 +2,13 @@
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
-
 "The sum of 4 and 7 is 11."
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
     let number = a + b;
+    console.log([number, 'The sum of ' + a + ' and ' + b + ' is ' + number + '.']);
     return [number, 'The sum of ' + a + ' and ' + b + ' is ' + number + '.'];
 
 }
@@ -22,18 +21,19 @@ testSum(4, 7);
 /////////////////////////////////////
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
 "The product of 5 and 9 is 45."
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    let number2 = a * b;
+    console.log([number2, 'The product of ' + a + ' and ' + b + ' is ' + number2 + '.']);
+    return [number2, 'The product of ' + a + ' and ' + b + ' is ' + number2 + '.'];
 }
 
+multiply(5,9);
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
