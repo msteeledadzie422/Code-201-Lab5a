0ed4a1e0893ac987b430b33cee5ae1130a1648a9
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-06-12 20:06:16 -0600

    Cleaning up sum branch

--- js/app.js
+++ js/app.js
@@ -30,14 +30,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    let number2 = a * b;
-    console.log([number2, 'The product of ' + a + ' and ' + b + ' is ' + number2 + '.']);
-    return [number2, 'The product of ' + a + ' and ' + b + ' is ' + number2 + '.'];
+  
 }
 
-multiply(5,9);
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5, 9);
+// testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -54,9 +51,6 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-    let number3 = 
-    let number4 = a * b * c;
-
 
 }
 
